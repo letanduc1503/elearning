d2a717b6abf62de362c90d9f256f87e6f135fb3a
letanduc1503 <118171797+letanduc1503@users.noreply.github.com>
2023-05-24 13:59:17 +0700

    Update SignUpPage.js

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,80 +1,79 @@
-import React from 'react'
-import Button from '../../components/Button/Button'
-import { message } from 'antd'
-import { SUCCESS } from '../../utils/constant'
-import { userServ } from '../../services/userServ'
-import Input from '../../components/Input/Input'
-import { useDispatch } from 'react-redux'
-import { Form, Formik } from 'formik'
-import { userLogin } from '../../toolkit/userSlice'
-export default function SignUpPage() {
+import React, { useState } from 'react'
+import {Button,Form,Input,InputNumber,Radio,} from 'antd';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { fetchApiUserRegister } from '../../toolkit/userSlice';
+
+export default function Register() {
   const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const onFinish = (values) => {
+    let newUser = {...values, maNhom: "GP00"}
+    const onSuccess = () => {
+      navigate("/login")
+    } 
+    dispatch(fetchApiUserRegister(newUser, onSuccess))
+  };
   return (
-    <Formik
-    firstName=''
-     lastName=''
-     email=''
-     password=''
-     confirmPassword=''
-     initialValues={{
-       firstName: "",
-       lastName: "",
-       email:"",
-       password:"",
-       confirmPassword:""
-     }}
-     onSubmit={async (values, actions) => {
-       try {
-         const result = await userServ.userLogin(values)
-         if (result.status === SUCCESS) {
-           dispatch(userLogin(result.data))
-           window.location.href = "/"
-           message.success("Đăng ký thành công")
-         }
-       } catch (err) {
-         message.error(err?.response?.data)
-       }
-     }}
-   >
-    
-     <Form style={{ backgroundImage: `url("./images/bg-authen.png")` }}
-    className='p-5 md:p-20 bg-red-300 rounded-lg bg-center z-10'>
-    <div className='text-center'>
-      <h1 className='font-bold text-xl md:text-3xl'>Đăng Ký</h1>
-      
+    <div className=''>
+      <h2 className='mb-4 laptop:mb-10 text-lg text-center font-bold laptop:text-2xl'>Đăng Ký Tài khoản</h2>
+      <Form   
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 20,
+        }}
+        layout="horizontal"
+        initialValues={{
+        }}
+        onFinish={onFinish}
+        validateTrigger= "onSubmit"
+        style={{
+        maxWidth: 600,
+      }}
+      >
+        <Form.Item
+          label="Họ tên"
+          name="hoTen"
+          rules={[{ required: true, message: 'Vui lòng nhập họ tên!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Tài khoản"
+          name="taiKhoan"
+          rules={[{ required: true, message: 'Vui lòng nhập tài khoản!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Mật khảu"
+          name="matKhau"
+          rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!'}, {min: 4, message: "Mật khảu ít nhất 4 ký tự"}]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ type: "email", required: true, message: 'Vui lòng nhập email!' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Số ĐT"
+          name="soDT"
+          rules={[{type: "string", message: "Vui lòng nhập số điện thoại"}, {pattern: /^0\d{9}$/, message: "Số điện thoại không hợp lệ"}]}
+        >
+          <Input />
+        </Form.Item>
+        <div className='text-center'>
+          <Button type='primary' htmlType="submit" className='bg-[#1677ff] mb-4 font-semibold text-sm'>Đăng ký</Button>
+        </div>
+      </Form>
+      <h2 className='text-center'>Bạn đã có tài khoản? <NavLink to="/login" className="text-red-500 font-semibold">Đăng nhập</NavLink></h2>
     </div>
-    <div className='mb-[30px]'>
-      <label htmlFor="firstName" className='form-label'>
-        First Name
-      </label>
-      <Input className="w-full md:w-[500px] lg:w-[800px]" name="firstName" type="text" placeholder="First Name" />
-    </div>
-    <div className='mb-[30px]'>
-      <label htmlFor="lastName" className='form-label'>Last Name</label>
-      <Input className="w-full md:w-[500px] lg:w-[800px]" name="lastName" type="text" placeholder='Last Name'>
-        
-      </Input>
-    </div>
-    <div className='mb-[30px]'>
-      <label htmlFor="email" className='form-label'>Email</label>
-      <Input className="w-full md:w-[500px] lg:w-[800px]" name="email" type="text" placeholder='Email'>
-        
-      </Input>
-    </div>
-    <div className='mb-[30px]'>
-      <label htmlFor="password" className='form-label'>Password</label>
-      <Input className="w-full md:w-[500px] lg:w-[800px]" name="password" type="text" placeholder='Password'>
-        
-      </Input>
-    </div>
-    <div className='mb-[30px]'>
-      <label htmlFor="confim-password" className='form-label'>Confim Password</label>
-      <Input className="w-full md:w-[500px] lg:w-[800px]" name="confirmpassword" type="text" placeholder='Confirmpassword'>
-        
-      </Input>
-    </div>
-    <Button type="submit" className="block bg-white mx-auto py-3 px-5 font-bold">Register</Button>
-  </Form>
-  </Formik>
   )
 }
+
