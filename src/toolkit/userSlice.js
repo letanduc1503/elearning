7d020cd913b872618e16c5276126326fb2130485
letanduc1503 <118171797+letanduc1503@users.noreply.github.com>
2023-05-24 14:04:49 +0700

    Update userSlice.js

--- src/toolkit/userSlice.js
+++ src/toolkit/userSlice.js
@@ -1,8 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { message } from 'antd';
+import { SUCCESS } from '../utils/constant';
 import { localUser } from '../utils/localUser';
+import { userServ } from '../services/userServ';
 
 const initialState = {
   userInfo: localUser.get(),
+
 }
 
 const userSlice = createSlice({
@@ -13,10 +17,33 @@ const userSlice = createSlice({
       state.userInfo = action.payload
       localUser.set(action.payload)
     },
+    addUser: (state, action) => {
+      state.userInfo = action.payload
+      localUser.set(action.payload)
+    },
 
   }
 });
 
+export const {addUser}=userSlice.actions
 export const { userLogin } = userSlice.actions
 
 export default userSlice.reducer
+
+
+
+export const fetchApiUserRegister = (userInfo, onSuccess)=>{
+  return async dispatch=>{
+    try{
+      const result = await userServ.userRegister(userInfo)
+      if (result.status === SUCCESS){
+        onSuccess()
+        message.success("Đăng ký tài khoản thành công")
+      }
+      } catch(err){
+        message.success("Đăng ký tài khoản thất bại")
+      
+    }
+  }
+}
+
